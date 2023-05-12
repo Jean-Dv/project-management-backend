ac24279ad7a977658bd6eb52552312da7a850565
Jean-Dv <mrjunior127@gmail.com>
2023-05-12 11:44:24 -0500

    test: :white_check_mark: added test for the update and delete

--- __test__/unit/project.spec.ts
+++ __test__/unit/project.spec.ts
@@ -1,5 +1,5 @@
 import { prismaMock } from "@root/prisma/singleton";
-import { createProject } from "@app/project/controller";
+import { createProject, deleteProject, updateProject } from "@app/project/controller";
 
 test('should create new project', async () => {
     const project = {
@@ -32,4 +32,44 @@ test('should throw error when creating new project', async () => {
     }
     prismaMock.project.create.mockRejectedValue(new Error("Error creating project"))
     await expect(createProject(project)).rejects.toThrow("Error creating project")
+})
+
+test('should update project', async () => {
+    const project = {
+        id: 1,
+        name: "Test Project Updated",
+        description: "Test Description Updated",
+        tasks: [],
+        updatedAt: new Date(),
+        createdAt: new Date()
+    }
+    prismaMock.project.update.mockResolvedValue(project)
+    await expect(updateProject(project)).resolves.toEqual({
+        id: 1,
+        name: "Test Project Updated",
+        description: "Test Description Updated",
+        tasks: [],
+        updatedAt: project.updatedAt,
+        createdAt: project.createdAt
+    })
+})
+
+test('should delete project', async () => {
+    const project = {
+        id: 1,
+        name: "Test Project deleted",
+        description: "Test Description deleted",
+        tasks: [],
+        updatedAt: new Date(),
+        createdAt: new Date()
+    }
+    prismaMock.project.delete.mockResolvedValue(project)
+    await expect(deleteProject(project.id)).resolves.toEqual({
+        id: 1,
+        name: "Test Project deleted",
+        description: "Test Description deleted",
+        tasks: [],
+        updatedAt: project.updatedAt,
+        createdAt: project.createdAt
+    })
 })
