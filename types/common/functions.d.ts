fdac6391e02be64421b3a823705ef8e15d7634ee
Jean-Dv <mrjunior127@gmail.com>
2023-05-12 20:39:45 -0500

    chore: :label: add types functions for the Tasks

--- types/common/functions.d.ts
+++ types/common/functions.d.ts
@@ -3,3 +3,8 @@ export type GetProject = (id: number) => Promise<Project>
 export type CreateProject = (project: ProjectInput) => Promise<Project>
 export type UpdateProject = (project: ProjectInput) => Promise<Project>
 export type DeleteProject = (id: number) => Promise<Project>
+export type GetTasks = (projectId: number) => Promise<Task[]>
+export type GetTask = (id: number) => Promise<Task>
+export type CreateTask = (task: TaskInput) => Promise<Task>
+export type UpdateTask = (task: TaskInput) => Promise<Task>
+export type DeleteTask = (id: number) => Promise<Task>
